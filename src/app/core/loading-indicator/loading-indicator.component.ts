08a7d6a771fd508f5fe7477f257e597e1dd28383
Miro <mir4ef@gmail.com>
2017-05-29 17:48:17 -0400

    updated the way unsubscribing from an Observalbe is handled on component destruction

--- src/app/core/loading-indicator/loading-indicator.component.ts
+++ src/app/core/loading-indicator/loading-indicator.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
+
+import 'rxjs/add/operator/takeUntil';
 
 import { LoadingIndicatorService } from './loading-indicator.service';
 
@@ -9,17 +11,20 @@ import { LoadingIndicatorService } from './loading-indicator.service';
   styleUrls: ['./loading-indicator.component.less']
 })
 export class LoadingIndicatorComponent implements OnDestroy {
-  private subscription: Subscription;
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
   public isLoading: boolean = false;
 
   constructor(private loaderIndicator: LoadingIndicatorService) {
-    this.subscription = this.loaderIndicator.getIndicatorState().subscribe(
+    this.loaderIndicator.getIndicatorState()
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe(
       state => this.isLoading = state,
       err => this.isLoading = false
     );
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
