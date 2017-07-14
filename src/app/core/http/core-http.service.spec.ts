6dba40c59f87b86d291c6cc552df71077256963e
Miro <mir4ef@gmail.com>
2017-07-14 00:35:48 -0400

    added more unit tests for the core http service

--- src/app/core/http/core-http.service.spec.ts
+++ src/app/core/http/core-http.service.spec.ts
@@ -40,12 +40,12 @@ describe('CoreHttpService', () => {
     });
   });
 
-  it('should exist', inject([CoreHttpService], (service: CoreHttpService) => {
+  it('should exist', inject([ CoreHttpService ], (service: CoreHttpService) => {
     expect(service).toBeTruthy();
   }));
 
   it('should GET requested endpoint and attach a token if present',
-    inject([CoreHttpService, TokenService, MockBackend], (service: CoreHttpService, tokenService: TokenService, backend: MockBackend) => {
+    inject([ CoreHttpService, TokenService, MockBackend ], (service: CoreHttpService, tokenService: TokenService, backend: MockBackend) => {
     tokenService.token = 'token string';
     const res: IResponse = { success: true, message: 'got data' };
     const response: ResponseOptions = new ResponseOptions({ body: JSON.stringify(res) });
@@ -68,8 +68,37 @@ describe('CoreHttpService', () => {
     });
   }));
 
+  it('should GET requested endpoint and attach the provided headers',
+    inject([ CoreHttpService, MockBackend ], (service: CoreHttpService, backend: MockBackend) => {
+      const headers = {
+        header1: 'value 1',
+        header2: 'value 2'
+      };
+      const res: IResponse = { success: true, message: 'got data' };
+      const response: ResponseOptions = new ResponseOptions({ body: JSON.stringify(res) });
+      const baseResponse: Response = new Response(response);
+
+      backend.connections.subscribe(
+        (c: MockConnection) => {
+          c.mockRespond(baseResponse);
+
+          expect(c.request.method).toBe(RequestMethod.Get);
+          expect(c.request.url).toBe(`/api/v1/endpoint`);
+          expect(c.request.headers.has('header1')).toBeTruthy();
+          expect(c.request.headers.get('header1')).toBe(headers.header1);
+          expect(c.request.headers.has('header2')).toBeTruthy();
+          expect(c.request.headers.get('header2')).toBe(headers.header2);
+        }
+      );
+
+      service.apiGet({ path: 'endpoint', headers }).subscribe(data => {
+        expect(data).toEqual(res);
+        expect(data.success).toBeTruthy();
+      });
+    }));
+
   it('should return 403 and navigate to login page if not authorized to access requested endpoint',
-    inject([Router, CoreHttpService, TokenService, MockBackend], (router: Router, service: CoreHttpService, tokenService: TokenService, backend: MockBackend) => {
+    inject([ Router, CoreHttpService, TokenService, MockBackend ], (router: Router, service: CoreHttpService, tokenService: TokenService, backend: MockBackend) => {
     const res: IResponse = { success: false, message: 'not authorized' };
     const response: ResponseOptions = new ResponseOptions({ type: ResponseType.Error, status: 403, body: JSON.stringify(res) });
     const baseResponse: ErrorResponse = new ErrorResponse(response);
@@ -96,7 +125,7 @@ describe('CoreHttpService', () => {
   }));
 
   describe('GET', () => {
-    it('should GET requested endpoint with success 200', inject([CoreHttpService, MockBackend], (service: CoreHttpService, backend: MockBackend) => {
+    it('should GET requested endpoint with success 200', inject([ CoreHttpService, MockBackend ], (service: CoreHttpService, backend: MockBackend) => {
       const res: IResponse = { success: true, message: 'got data' };
       const response: ResponseOptions = new ResponseOptions({ body: JSON.stringify(res) });
       const baseResponse: Response = new Response(response);
@@ -116,7 +145,7 @@ describe('CoreHttpService', () => {
       });
     }));
 
-    it('should GET requested endpoint with query params and success 200', inject([CoreHttpService, MockBackend], (service: CoreHttpService, backend: MockBackend) => {
+    it('should GET requested endpoint with query params and success 200', inject([ CoreHttpService, MockBackend ], (service: CoreHttpService, backend: MockBackend) => {
       const res: IResponse = { success: true, message: 'got data' };
       const response: ResponseOptions = new ResponseOptions({ body: JSON.stringify(res) });
       const baseResponse: Response = new Response(response);
@@ -136,7 +165,7 @@ describe('CoreHttpService', () => {
       });
     }));
 
-    it('should GET requested endpoint with specified schema and url and success 200', inject([CoreHttpService, MockBackend], (service: CoreHttpService, backend: MockBackend) => {
+    it('should GET requested endpoint with specified schema and url and success 200', inject([ CoreHttpService, MockBackend ], (service: CoreHttpService, backend: MockBackend) => {
       const res: IResponse = { success: true, message: 'got data' };
       const response: ResponseOptions = new ResponseOptions({ body: JSON.stringify(res) });
       const baseResponse: Response = new Response(response);
@@ -156,7 +185,7 @@ describe('CoreHttpService', () => {
       });
     }));
 
-    it('should GET requested endpoint with specified schema, url and port and success 200', inject([CoreHttpService, MockBackend], (service: CoreHttpService, backend: MockBackend) => {
+    it('should GET requested endpoint with specified schema, url and port and success 200', inject([ CoreHttpService, MockBackend ], (service: CoreHttpService, backend: MockBackend) => {
       const res: IResponse = { success: true, message: 'got data' };
       const response: ResponseOptions = new ResponseOptions({ body: JSON.stringify(res) });
       const baseResponse: Response = new Response(response);
@@ -176,7 +205,7 @@ describe('CoreHttpService', () => {
       });
     }));
 
-    it('should GET requested endpoint with specified schema, url and no path and success 200', inject([CoreHttpService, MockBackend], (service: CoreHttpService, backend: MockBackend) => {
+    it('should GET requested endpoint with specified schema, url and no path and success 200', inject([ CoreHttpService, MockBackend ], (service: CoreHttpService, backend: MockBackend) => {
       const res: IResponse = { success: true, message: 'got data' };
       const response: ResponseOptions = new ResponseOptions({ body: JSON.stringify(res) });
       const baseResponse: Response = new Response(response);
@@ -196,7 +225,7 @@ describe('CoreHttpService', () => {
       });
     }));
 
-    it('should GET requested endpoint with failure 500', inject([CoreHttpService, MockBackend], (service: CoreHttpService, backend: MockBackend) => {
+    it('should GET requested endpoint with failure 500', inject([ CoreHttpService, MockBackend ], (service: CoreHttpService, backend: MockBackend) => {
       const res: IResponse = { success: false, message: 'got error' };
       const response: ResponseOptions = new ResponseOptions({ type: ResponseType.Error, status: 500, body: JSON.stringify(res) });
       const baseResponse: ErrorResponse = new ErrorResponse(response);
@@ -218,7 +247,7 @@ describe('CoreHttpService', () => {
   });
 
   describe('POST', () => {
-    it('should POST requested endpoint with success 200', inject([CoreHttpService, MockBackend], (service: CoreHttpService, backend: MockBackend) => {
+    it('should POST requested endpoint with success 200', inject([ CoreHttpService, MockBackend ], (service: CoreHttpService, backend: MockBackend) => {
       const body = { key: 'data for post' };
       const res: IResponse = { success: true, message: 'got data' };
       const response: ResponseOptions = new ResponseOptions({ body: JSON.stringify(res) });
@@ -240,7 +269,7 @@ describe('CoreHttpService', () => {
       });
     }));
 
-    it('should POST requested endpoint with no body and success 200', inject([CoreHttpService, MockBackend], (service: CoreHttpService, backend: MockBackend) => {
+    it('should POST requested endpoint with no body and success 200', inject([ CoreHttpService, MockBackend ], (service: CoreHttpService, backend: MockBackend) => {
       const res: IResponse = { success: true, message: 'got data' };
       const response: ResponseOptions = new ResponseOptions({ body: JSON.stringify(res) });
       const baseResponse: Response = new Response(response);
@@ -261,7 +290,7 @@ describe('CoreHttpService', () => {
       });
     }));
 
-    it('should POST requested endpoint with failure 500', inject([CoreHttpService, MockBackend], (service: CoreHttpService, backend: MockBackend) => {
+    it('should POST requested endpoint with failure 500', inject([ CoreHttpService, MockBackend ], (service: CoreHttpService, backend: MockBackend) => {
       const body = { key: 'data for post' };
       const res: IResponse = { success: false, message: 'got error' };
       const response: ResponseOptions = new ResponseOptions({ body: JSON.stringify(res) });
